6f2a88d5d6b35a2d69c5a5a9a2bcce2e2890d05a
mskarabo <mihael.skarabot@gmail.com>
2015-10-26 20:34:07 +0100

    updated config

--- config.cmd
+++ config.cmd
@@ -1,7 +1,7 @@
 @set DEV_HOME=C:\adev
-@set JAVA_HOME=%DEV_HOME%\jdk\jdk180_25
+@set JAVA_HOME=%DEV_HOME%\jdk\jdk1.8.0_51
 @set JBOSS_HOME=%DEV_HOME%\server\wildfly-8.2.0.Final
-@set M2_HOME=%DEV_HOME%\utils\apache-maven-3.2.3
+@set M2_HOME=%DEV_HOME%\utils\apache-maven-3.3.3
 @set WILDFLY_HOME=%JBOSS_HOME%
 @set PATH=%M2_HOME%\bin;%PATH%
 @set PROJECT_DIR=%~dp0\indiana-core
